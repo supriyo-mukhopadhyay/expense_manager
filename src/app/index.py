3adfb45abd9a317b4d4c0c8f481211254c96126c
Supriyo Mukhopadhyay <smukhopadhyay@grantengineering.ie>
2026-07-10 14:23:39 +0100

    session managemnt

--- src/app/index.py
+++ src/app/index.py
@@ -15,10 +15,11 @@ app = Flask(
     static_folder=f"{projectPath}/src/app/static",
 )
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-app.config["SESSION_PERMANENT"] = False     # Sessions expire when the browser is closed
-app.config["SESSION_TYPE"] = "filesystem"     # Store session data in files
+app.config["SESSION_PERMANENT"] = False  # Sessions expire when the browser is closed
+app.config["SESSION_TYPE"] = "filesystem"  # Store session data in files
 # Initialize Flask-Session
 Session(app)
+
 
 @app.route("/", methods=["GET", "POST"])
 def loginForm():
@@ -32,7 +33,7 @@ def loginForm():
             session["user_id"] = userData[0]
             session["user_name"] = userData[1]
             session["email"] = userData[2]
-            
+
             return render_template("dashboard.html", userData=userData)
     return render_template("login.html")
 
@@ -51,26 +52,86 @@ def addExpenseForm():
 
         db.saveExpense(formData, session["user_id"])
     categories = db.getCategories()
-    
+
     return render_template("addexpense.html", categories=categories)
 
 
-
-@app.route("/reports", methods=["POST"])
+@app.route("/reports", methods=["GET", "POST"])
 def renderReports():
     if not session.get("user_id"):
         return redirect("/")
-    # return render_template("reports.html")
-    interval = None
-    if request.method == "POST":
-        # print("hi")
-        interval = request.form["interval"]
-        # print(request.form)
-        pt.plot(interval)
-    else:
-        pt.plot()
+    try:
+        if request.method == "POST":
+            interval = request.form.get("interval")
+            pt.plot(interval)  # type: ignore
+            print(interval)
+    except Exception as e:
+        print(e)
     return render_template("reports.html")
 
+
+@app.route("/DailyReports", methods=["GET"])
+def renderDailyReports():
+    if not session.get("user_id"):
+        return redirect("/")
+    try:
+        if request.method == "GET":
+            pt.plot("daily")
+        else:
+            pt.plot()
+    except Exception as e:
+        print(e)
+    return render_template("reports.html")
+
+
+@app.route("/weeklyReports", methods=["GET"])
+def renderWeeklyReports():
+    if not session.get("user_id"):
+        return redirect("/")
+    try:
+        if request.method == "GET":
+            pt.plot("weekly")
+        else:
+            pt.plot()
+    except Exception as e:
+        print(e)
+    return render_template("reports.html")
+
+
+@app.route("/monthlyReports", methods=["GET"])
+def renderMonthlyReports():
+    if not session.get("user_id"):
+        return redirect("/")
+    try:
+        if request.method == "GET":
+            pt.plot("monthly")
+        else:
+            pt.plot()
+    except Exception as e:
+        print(e)
+    return render_template("reports.html")
+
+
+@app.route("/save", methods=["GET", "POST"])
+def save():
+    if not session.get("user_id"):
+        return redirect("/")
+    print("hi")
+    return render_template("dashboard.html")
+
+
+@app.route("/yearlyReports", methods=["GET"])
+def renderYearlyReports():
+    if not session.get("user_id"):
+        return redirect("/")
+    try:
+        if request.method == "GET":
+            pt.plot("yearly")
+        else:
+            pt.plot()
+    except Exception as e:
+        print(e)
+    return render_template("reports.html")
 
 
 @app.route("/user", methods=["GET", "POST"])
@@ -79,10 +140,12 @@ def userDetails():
         return redirect("/")
     fname = session["user_name"].split()[0]
     lname = session["user_name"].split()[1]
-    
+
     email = session["email"]
     id = session["user_id"]
-    return render_template("userdetails.html", fname=fname, lname=lname, email_addr=email, id=id)
+    return render_template(
+        "userdetails.html", fname=fname, lname=lname, email_addr=email, id=id
+    )
 
 
 @app.route("/home", methods=["GET", "POST"])
@@ -92,40 +155,13 @@ def home():
     return render_template("dashboard.html")
 
 
-
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
     session["user_id"] = None
     session["user_name"] = None
     session["email"] = None
-    
+
     return redirect("/")
-
-
-# @app.route("/index", methods=["GET", "POST"])
-# def enpenseForm():
-#     if request.method == "POST":
-#         formData = request.form.to_dict(flat=True)
-#         print(formData)
-#         if request.form["category_id"] == "other":
-#             category_id = db.insertCategory(request.form["other"])
-#             formData["category_id"] = category_id  # type: ignore
-#             print(formData)
-
-#         db.saveExpense(formData)
-#     categories = db.getCategories()
-#     return render_template("index.html", categories=categories)
-
-
-# @app.route("/report", methods=["GET", "POST"])
-# def report():
-#     interval = None
-#     if request.method == "POST":
-#         interval = request.form["interval"]
-#         pt.plot(interval)
-#     else:
-#         pt.plot()
-#     return render_template("report.html", interval=interval)
 
 
 if __name__ == "__main__":
--- src/app/visualization/plots.py
+++ src/app/visualization/plots.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import os
+
+from numpy import size
 from model import data
+
+plt.style.use("bmh")
 
 
 class report:
@@ -18,7 +22,12 @@ class report:
             amount.append(expense["amount"])
 
         fig, ax = plt.subplots()
-        ax.pie(amount, labels=categories)
-        ax.axis("equal")
+        # ax.pie(amount, labels=categories)
+        # ax.axis("equal")
+        ax.bar(categories, amount, label=categories)
         # plt.show()
         return fig.savefig(f"{self.projectPath}/src/app/static/img/expenses.png")
+
+
+# re = report()
+# re.plot()
